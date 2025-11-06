fb0a99820647b1e82952f2780344c50803680034
Erik <erik.ehrenburg@gmail.com>
2025-11-06 14:52:23 +0000

    created operational_margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,16 @@
+-- int_orders_operational.sql
+
+select
+  o.orders_id
+  , o.date_date
+  , round(o.margin + s.shipping_fee - (s.logcost + s.ship_cost), 2) as operational_margin
+  , o.quantity
+  , o.purchase_cost
+  , o.margin
+  , s.shipping_fee
+  , s.logcost
+  , s.ship_cost
+from {{ref("int_orders_margin")}} o
+left join {{ref("stg_raw__ship")}} s
+  using(orders_id)
+order by orders_id desc 
