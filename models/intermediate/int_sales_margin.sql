e1e4b42e9d79379fd6cc88b0b83b16ec61bb6fef
Erik <erik.ehrenburg@gmail.com>
2025-11-06 13:18:02 +0000

    corrected typos in "product_id" vs "products_id"

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,14 @@
+-- models/intermetiate/int_sales_margin.sql
+
+select
+  s.products_id
+  , s.date_date
+  , s.orders_id
+  , s.revenue
+  , s.quantity
+  , p.purchase_price
+, round(s.quantity * p.purchase_price, 2) as purchase_cost
+, round(s.revenue - s.quantity * p.purchase_price, 2) as margin 
+from {{ref('stg_raw__sales')}} s
+left join {{ref('stg_raw__product')}} p
+  using (products_id)
